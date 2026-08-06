4133c2044bc9e9207889de92338250c05143033e
KacperBrzymyszkiewicz <brzymyszkiewiczkacper@gmail.com>
2026-08-06 13:40:21 +0200

    new data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import requests
+import soccerdata as sd
 from narwhals import DataFrame
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -11,27 +12,19 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 team_name = "Lech Poznan"
 df = pd.read_csv("./POL.csv")
-do_fetch = False
-api_key = "469f419a716fc8e85530a08c161c75cc"
-url ="https://v3.football.api-sports.io"
-headers = {"x-apisports-key": api_key}
-params = {"league":"106","season":"2024"}
-if do_fetch == True:
-    response = requests.get(f"{url}/teams", params=params, headers=headers)
-    response = response.json()
-    print(response)
-    with open("league_data.json","w",encoding="utf-8") as file:
-        file.write(json.dumps(response))
 
-    with open("league_data.json","r",encoding="utf-8") as file:
-        data = json.load(file)
-    print(data)
-else:
-    with open("league_data.json","r",encoding="utf-8") as file:
-        data=json.load(file)
-    print(data)
 #print(response)
 #print(df)
+data = sd.FBref(
+    leagues="POL-Ekstraklasa",
+    seasons="2025"
+)
+stats = data.read_schedule()
+print(stats)
+class teamstats:
+    def __init__(self,data):
+        self.Goals = data
+
 def all_matches(team_name):
     teamstats = df.loc[(df["Season"]=="2025/2026") & ((df["Home"] == team_name) | (df["Away"] == team_name)),["Home","Away","HG","AG","Res"]].copy()
     teamstats["Goals"] = np.where(
@@ -45,12 +38,9 @@ def all_matches(team_name):
     return teamstats
 
 
-teamstats = all_matches(team_name)
 
-
-
-a,b = np.polyfit( teamstats["MatchNumber"],teamstats["Goals"], 1)
-trend = a * teamstats["MatchNumber"] + b
+#a,b = np.polyfit( teamstats["MatchNumber"],teamstats["Goals"], 1)
+#trend = a * teamstats["MatchNumber"] + b
 #print(teamstats[teamstats["Res"] == "Win"]["Goals"].sum()/(teamstats["Res"]=="Win").sum())
 # print(a,b)
 # print(teamstats[["Goals","MatchNumber"]])
